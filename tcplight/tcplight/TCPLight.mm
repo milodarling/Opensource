2d4264b9ee930d1e3881ac95579953f2ac7bd20e
Riley Durant <openro0t@Rileys-iMac.local>
2015-01-01 14:37:43 -0500

    Cuter preference bundle, still not fully functional

--- tcplight/tcplight/TCPLight.mm
+++ tcplight/tcplight/TCPLight.mm
@@ -11,6 +11,19 @@
 	}
 	return _specifiers;
 }
+
+-(void)openTwitter {
+	if 	([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"tweetbot:"]])
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"tweetbot:///user_profile/openro0t"]];
+    else if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"twitter:"]])
+    	[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"twitter://user?screen_name=openro0t"]];
+    else if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"safari:"]])
+    		[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://mobile.twitter.com/openro0t"]];
+}
+
+-(void)openGit {
+		[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://github.com/openro0t/Opensource/tree/master/tcplight"]];
+}
 @end
 
-// vim:ft=objc
+//vim:ft=objc
